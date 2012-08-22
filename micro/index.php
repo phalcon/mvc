007783054725958566f096661d3494effac47ab1
phalcon <andres@phalconphp.com>
2012-08-22 03:53:08 -0500

    Removing  because it doesn't works in php 5.3

--- micro/index.php
+++ micro/index.php
@@ -6,16 +6,22 @@ $app->get('/', function () {
     echo "<h1>Welcome!</h1>";
 });
 
-$app->get('/say/hello/{name}', function ($name) {
+$app->get('/say/hello/{name}', function ($name) use ($app) {
     echo "<h1>Hello! $name</h1>";
+    echo "Your IP Address is ", $app->request->getClientAddress();
 });
 
-$app->post('/store/something', function () {
+$app->post('/store/something', function () use ($app) {
 
-    $name = $this->request->getPost('name');
+    $name = $app->request->getPost('name');
 
     echo "<h1>Hello! $name</h1>";
     
+});
+
+$app->notFound(function () use ($app) {
+	$app->response->setStatusCode(404, "Not Found")->sendHeaders();
+	echo 'This is crazy, but this page was not found!';
 });
 
 $app->handle();
