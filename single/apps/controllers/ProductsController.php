3f64d454dfc5cb9cea42986864ad333bc369a8d4
phalcon <eduar@phalconphp.com>
2012-09-11 20:38:30 -0500

    Add test setRenderLevel

--- single/apps/controllers/ProductsController.php
+++ single/apps/controllers/ProductsController.php
@@ -2,8 +2,14 @@
 
 class ProductsController extends \Phalcon\Mvc\Controller {
 	
-	public function indexAction(){
+	public function indexAction()
+	{
 		$this->view->setVar('product', Products::findFirst());
+	}
+
+	public function testAction()
+	{
+		$this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_NO_RENDER);
 	}
 
 }
