de2b5c94c8c08654109c1e19708ae9631a02f829
Sid Roberts <sid@sidroberts.co.uk>
2016-09-10 12:36:28 +0100

    Tidied up the Micro example.

--- micro/index.php
+++ micro/index.php
@@ -4,25 +4,39 @@ use Phalcon\Mvc\Micro;
 
 $app = new Micro();
 
-$app->get('/', function () {
-    echo "<h1>Welcome!</h1>";
-});
+$app->get(
+    "/",
+    function () {
+        echo "<h1>Welcome!</h1>";
+    }
+);
 
-$app->get('/say/hello/{name}', function ($name) use ($app) {
-    echo "<h1>Hello! $name</h1>";
-    echo "Your IP Address is ", $app->request->getClientAddress();
-});
+$app->get(
+    "/say/hello/{name}",
+    function ($name) use ($app) {
+        echo "<h1>Hello! $name</h1>";
 
-$app->post('/store/something', function () use ($app) {
+        echo "Your IP Address is ", $app->request->getClientAddress();
+    }
+);
 
-    $name = $app->request->getPost('name');
+$app->post(
+    "/store/something",
+    function () use ($app) {
+        $name = $app->request->getPost("name");
 
-    echo "<h1>Hello! $name</h1>";
-});
+        echo "<h1>Hello! $name</h1>";
+    }
+);
 
-$app->notFound(function () use ($app) {
-    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
-    echo 'This is crazy, but this page was not found!';
-});
+$app->notFound(
+    function () use ($app) {
+        $app->response->setStatusCode(404, "Not Found");
+
+        $app->response->sendHeaders();
+
+        echo "This is crazy, but this page was not found!";
+    }
+);
 
 $app->handle();
